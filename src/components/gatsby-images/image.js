7b79d8c35a20c81ce6d07d2c9cc32a64e9fb5a36
Bravoo Sonja <bravoosonja@gmail.com>
2022-06-20 20:05:52 +1000

    Start menu

--- src/components/gatsby-images/image.js
+++ src/components/gatsby-images/image.js
@@ -2,17 +2,6 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 
-/*
- * This component is built using `gatsby-image` to automatically serve optimized
- * images with lazy loading and reduced file sizes. The image is loaded using a
- * `useStaticQuery`, which allows us to load the image from directly within this
- * component, rather than having to pass the image data down from pages.
- *
- * For more information, see the docs:
- * - `gatsby-image`: https://gatsby.dev/gatsby-image
- * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 export const Image = ({ src }) => {
   const data = useStaticQuery(graphql`
     query {
@@ -74,7 +63,7 @@ export const Image = ({ src }) => {
       ) : src === 5 ? (
         <Img fluid={data.project5.childImageSharp.fluid} />
       ) : (
-        <Img fluid={data.project5.childImageSharp.fluid} />
+        <Img fluid={data.project6.childImageSharp.fluid} />
       )}
     </>
   )
--- src/components/menu.js
+++ src/components/menu.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import { Close } from "../icons/icons"
 // components
 import { Image } from "./gatsby-images/image"
+import data from "../data/projects.json"
 
 const Menu = () => {
   return (
@@ -16,35 +17,56 @@ const Menu = () => {
         <div className="container">
           <div className="menu-inner">
             <ul>
-              <li>
-                <Link to={`/projects/a`}>
-                  <div className="wrapper">
-                    <div className="line left">
-                      {/* <div className="mask"></div> */}
-                    </div>
-                    <div className="title">
-                      <h2>
-                        <div className="text">Shopping Cart</div>
-                      </h2>
-                    </div>
-                    <div className="thumbnail">
-                      <Image src={1} />
-                      {/* <div className="mask"></div> */}
-                    </div>
-                    <div className="floating-image">
-                      <Image src={1} />
-                    </div>
-                    <div className="line right">
-                      {/* <div className="mask right"></div> */}
-                    </div>
-                  </div>
-                </Link>
-              </li>
+              {data.map(list => (
+                <List
+                  key={list.id}
+                  title={list.title}
+                  src={list.src}
+                  leftLineFlex={list.leftLineFlex}
+                  rightLineFlex={list.rightLineFlex}
+                  thumbnailPosition={list.thumbnailPosition}
+                />
+              ))}
             </ul>
           </div>
         </div>
       </div>
     </div>
+  )
+}
+
+const List = ({
+  title,
+  src,
+  leftLineFlex,
+  rightLineFlex,
+  thumbnailPosition,
+}) => {
+  return (
+    <li>
+      <Link to={`/projects/a`}>
+        <div className="wrapper">
+          <div className={`line left flex-${leftLineFlex}`}>
+            {/* <div className="mask"></div> */}
+          </div>
+          <div className="title">
+            <h2>
+              <div className="text">{title}</div>
+            </h2>
+          </div>
+          <div className="thumbnail" style={{ left: thumbnailPosition }}>
+            <Image src={src} />
+            {/* <div className="mask"></div> */}
+          </div>
+          <div className="floating-image">
+            <Image src={src} />
+          </div>
+          <div className={`line right flex-${rightLineFlex}`}>
+            {/* <div className="mask right"></div> */}
+          </div>
+        </div>
+      </Link>
+    </li>
   )
 }
 
